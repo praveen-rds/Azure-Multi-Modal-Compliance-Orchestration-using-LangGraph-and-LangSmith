64a4f33a294269fc1a06bb6cf14ebcceb2b578f3
praavi <praavi@uwaterloo.ca>
2026-04-15 10:58:35 -0400

    Add document indexer, video indexer service, and graph workflow

---
+++ backend/src/graph/workflow.py
@@ -0,0 +1,38 @@
+'''
+This modeule defines the DAG : Directed Acyclic Graph that orchestrates the video compliance
+audit process.
+it connects the nodes using the StateGraph from LangGraph
+
+START -> index_video_node -> audit_content_node -> END
+'''
+
+from langgraph.graph import StateGraph, END
+from backend.src.graph.state import VideoAuditState
+
+from backend.src.graph.nodes import(
+    index_video_node,
+    audio_content_node
+)
+
+def create_graph():
+    '''
+    Constructs and compiles the Langgraph workflow
+    Returns :
+    Compiled Graph: runnable graph or execution
+    '''
+    #initialize the graph with state schema
+    workflow = StateGraph(VideoAuditState)
+    #add the nodes
+    workflow.add.node("indexer", index_video_node)
+    workflow.add.node("auditor", audio_content_node)
+    #define the netry point : indes=xer
+    workflow.set_entry_point("indexer")
+    #define the edges
+    workflow.add.edge("indexer", "auditor")
+    workflow.add.edge  ("auditor", END)
+    #compile the graph
+    app = workflow.compile()
+    return   app
+
+#expose the runnable app
+app = create_graph()
